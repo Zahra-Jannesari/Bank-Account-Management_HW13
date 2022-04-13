bb41df520ca4adc961a9a8c7d4abb1c9818765f2
Zarisa <jannesari.zarisa@gmail.com>
2022-04-13 20:57:37 +0430

    set up logic for ViewProfileFragment.kt

--- app/src/main/java/com/zarisa/bankaccountmanagement/ViewProfileFragment.kt
+++ app/src/main/java/com/zarisa/bankaccountmanagement/ViewProfileFragment.kt
@@ -1,62 +1,59 @@
 package com.zarisa.bankaccountmanagement
 
 import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import com.zarisa.bankaccountmanagement.databinding.FragmentViewProfileBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ViewProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ViewProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    lateinit var binding: FragmentViewProfileBinding
+    private val profileSharePref: SharedPreferences by lazy {
+        requireActivity().getSharedPreferences(
+            "user Information",
+            Context.MODE_PRIVATE
+        )
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 //        val actionBar = (requireActivity().actionBar)
 //        actionBar?.setDisplayHomeAsUpEnabled(false)
-        return inflater.inflate(R.layout.fragment_view_profile, container, false)
+        binding = FragmentViewProfileBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ViewProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ViewProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
     }
+
+    private fun initViews() {
+        putData()
+        binding.editButton.setOnClickListener {
+            editTime=true
+            findNavController().navigate(R.id.action_viewProfileFragment_to_profileFragment)
+        }
+    }
+
+    private fun putData() {
+        binding.EditTextUserName.setText(profileSharePref.getString(userName,""))
+        binding.EditTextUserLastname.setText(profileSharePref.getString(lastName,""))
+        binding.EditTextUserFatherName.setText(profileSharePref.getString(father,""))
+        binding.EditTextUserZipCode.setText(profileSharePref.getString(zipCode,""))
+        binding.EditTextUserPhoneNumber.setText(profileSharePref.getString(phoneNumber,""))
+        binding.EditTextUserAccountNumber.setText(numberOfUserAccounts.toString())
+    }
+
+
 }
