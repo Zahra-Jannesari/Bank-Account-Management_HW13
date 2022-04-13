7c6cdefdae8430d8d10ed57baa153c6547c21cbf
Zarisa <jannesari.zarisa@gmail.com>
2022-04-13 20:35:28 +0430

    put data for edit in editTexts and organize the code

--- app/src/main/java/com/zarisa/bankaccountmanagement/ProfileFragment.kt
+++ app/src/main/java/com/zarisa/bankaccountmanagement/ProfileFragment.kt
@@ -10,34 +10,41 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.zarisa.bankaccountmanagement.databinding.FragmentProfileBinding
-const val userName="name"
-const val lastName="lastname"
-const val father="fatherName"
-const val zipCode="zipCode"
-const val phoneNumber="phone"
-var numberOfUserAccounts:Int=0
-var editTime=true
+
+const val userName = "name"
+const val lastName = "lastname"
+const val father = "fatherName"
+const val zipCode = "zipCode"
+const val phoneNumber = "phone"
+var numberOfUserAccounts: Int = 0
+var editTime = true
+
 class ProfileFragment : Fragment() {
-    private val profileSharePref: SharedPreferences by lazy { requireActivity().getSharedPreferences("user Information", Context.MODE_PRIVATE) }
+    private val profileSharePref: SharedPreferences by lazy {
+        requireActivity().getSharedPreferences(
+            "user Information",
+            Context.MODE_PRIVATE
+        )
+    }
     lateinit var binding: FragmentProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         choseCorrectFragment()
-        binding= FragmentProfileBinding.inflate(layoutInflater, container, false)
+        binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     private fun choseCorrectFragment() {
-        if (profileSharePref.getString(userName,"")!=""&&!editTime)
+        if (profileSharePref.getString(userName, "") != "" && !editTime)
             findNavController().navigate(R.id.action_profileFragment_to_viewProfileFragment)
         else return
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        putDataInEditTexts()
+        putDataInEditTexts()
         initViews()
     }
 
@@ -51,72 +58,76 @@ class ProfileFragment : Fragment() {
     }
 
     private fun saveEditTextsData() {
-        if(validateData()) {
-            editTime=false
+        if (validateData()) {
+            editTime = false
             saveData()
             findNavController().navigate(R.id.action_profileFragment_to_viewProfileFragment)
-        }else
-            Toast.makeText(requireContext(),"لطفا تمامی اطلاعات را بطور صحیح تکمیل کنید.",Toast.LENGTH_SHORT).show()
+        } else
+            Toast.makeText(
+                requireContext(),
+                "لطفا تمامی اطلاعات را بطور صحیح تکمیل کنید.",
+                Toast.LENGTH_SHORT
+            ).show()
     }
 
     private fun saveData() {
         val editor = profileSharePref.edit()
-        editor.putString(userName,binding.EditTextUserName.text.toString())
-        editor.putString(lastName,binding.EditTextUserLastname.text.toString())
-        editor.putString(father,binding.EditTextUserFatherName.text.toString())
-        editor.putString(zipCode,binding.EditTextUserZipCode.text.toString())
-        editor.putString(phoneNumber,binding.EditTextUserPhoneNumber.text.toString())
+        editor.putString(userName, binding.EditTextUserName.text.toString())
+        editor.putString(lastName, binding.EditTextUserLastname.text.toString())
+        editor.putString(father, binding.EditTextUserFatherName.text.toString())
+        editor.putString(zipCode, binding.EditTextUserZipCode.text.toString())
+        editor.putString(phoneNumber, binding.EditTextUserPhoneNumber.text.toString())
         editor?.apply()
-        numberOfUserAccounts=binding.EditTextUserAccountNumber.text.toString().toInt()
+        numberOfUserAccounts = binding.EditTextUserAccountNumber.text.toString().toInt()
     }
 
     private fun validateData(): Boolean {
-        var areAllDataValid =true
+        var areAllDataValid = true
         binding.EditTextUserName.let {
-            if (it.text.isNullOrBlank()){
-                areAllDataValid=false
+            if (it.text.isNullOrBlank()) {
+                areAllDataValid = false
                 it.error = "تکمیل فیلد اجباری است!"
             }
         }
         binding.EditTextUserLastname.let {
-            if (it.text.isNullOrBlank()){
-                areAllDataValid=false
+            if (it.text.isNullOrBlank()) {
+                areAllDataValid = false
                 it.error = "تکمیل فیلد اجباری است!"
             }
         }
         binding.EditTextUserFatherName.let {
-            if (it.text.isNullOrBlank()){
-                areAllDataValid=false
+            if (it.text.isNullOrBlank()) {
+                areAllDataValid = false
                 it.error = "تکمیل فیلد اجباری است!"
             }
         }
         binding.EditTextUserPhoneNumber.let {
-            if (it.text.isNullOrBlank()){
-                areAllDataValid=false
+            if (it.text.isNullOrBlank()) {
+                areAllDataValid = false
                 it.error = "تکمیل فیلد اجباری است!"
             }
-            if (it.text.toString().length!=11){
-                areAllDataValid=false
+            if (it.text.toString().length != 11) {
+                areAllDataValid = false
                 it.error = "شماره تلفن نامعتبر است!"
             }
         }
         binding.EditTextUserZipCode.let {
-            if (it.text.isNullOrBlank()){
-                areAllDataValid=false
+            if (it.text.isNullOrBlank()) {
+                areAllDataValid = false
                 it.error = "تکمیل فیلد اجباری است!"
             }
-            if (it.text.toString().length!=10){
-                areAllDataValid=false
+            if (it.text.toString().length != 10) {
+                areAllDataValid = false
                 it.error = "کدپستی نامعتبر است!"
             }
         }
         binding.EditTextUserAccountNumber.let {
-            if (it.text.isNullOrBlank()){
-                areAllDataValid=false
+            if (it.text.isNullOrBlank()) {
+                areAllDataValid = false
                 it.error = "تکمیل فیلد اجباری است!"
             }
-            if (it.text.toString()=="0"){
-                areAllDataValid=false
+            if (it.text.toString() == "0") {
+                areAllDataValid = false
                 it.error = "تعداد حساب ها را بطور صحیح وارد کنید!"
             }
         }
@@ -124,13 +135,13 @@ class ProfileFragment : Fragment() {
         return areAllDataValid
     }
 
-//    private fun putDataInEditTexts() {
-//        binding.EditTextUserName.setText(profileSharePref.getString(userName,""))
-//        binding.EditTextUserLastname.setText(profileSharePref.getString(lastName,""))
-//        binding.EditTextUserFatherName.setText(profileSharePref.getString(father,""))
-//        binding.EditTextUserZipCode.setText(profileSharePref.getString(zipCode,""))
-//        binding.EditTextUserPhoneNumber.setText(profileSharePref.getString(phoneNumber,""))
-//        if(numberOfUserAccounts>0)
-//            binding.EditTextUserAccountNumber.setText(numberOfUserAccounts)
-//    }
+    private fun putDataInEditTexts() {
+        binding.EditTextUserName.setText(profileSharePref.getString(userName,""))
+        binding.EditTextUserLastname.setText(profileSharePref.getString(lastName,""))
+        binding.EditTextUserFatherName.setText(profileSharePref.getString(father,""))
+        binding.EditTextUserZipCode.setText(profileSharePref.getString(zipCode,""))
+        binding.EditTextUserPhoneNumber.setText(profileSharePref.getString(phoneNumber,""))
+        if(numberOfUserAccounts>0)
+            binding.EditTextUserAccountNumber.setText(numberOfUserAccounts.toString())
+    }
 }
