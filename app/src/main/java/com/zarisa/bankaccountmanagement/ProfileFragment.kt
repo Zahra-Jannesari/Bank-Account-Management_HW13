e8c55eb5f713a1093541b5d2eace9673d07b3957
Zarisa <jannesari.zarisa@gmail.com>
2022-04-13 11:09:40 +0430

    initialize the logic for profile fragment

--- app/src/main/java/com/zarisa/bankaccountmanagement/ProfileFragment.kt
+++ app/src/main/java/com/zarisa/bankaccountmanagement/ProfileFragment.kt
@@ -1,26 +1,46 @@
 package com.zarisa.bankaccountmanagement
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.zarisa.bankaccountmanagement.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
-
-
+    val profileSharePref by lazy { requireActivity().getSharedPreferences("user Information", Context.MODE_PRIVATE) }
+    lateinit var binding: FragmentProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        binding= FragmentProfileBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        putDataInEditTexts()
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.saveChangeButton.setOnClickListener { saveEditTextsData() }
+    }
+
+    private fun saveEditTextsData() {
+        if(validateData()) {
+            TODO("Not yet implemented")
+        }
+    }
+
+    private fun validateData(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    private fun putDataInEditTexts() {
+        TODO("Not yet implemented")
     }
 }
-
-//// Get input text
-//val inputText = outlinedTextField.editText?.text.toString()
-//
-//outlinedTextField.editText?.doOnTextChanged { inputText, _, _, _ ->
-//    // Respond to input text change
-//}
