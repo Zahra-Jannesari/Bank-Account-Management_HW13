7add9a37588b29d90aa437e819e95e75dd45feb9
Zarisa <jannesari.zarisa@gmail.com>
2022-04-13 13:53:18 +0430

    new Dao for dataBase

---
+++ app/src/main/java/com/zarisa/bankaccountmanagement/data_base/DaoDataBase.kt
@@ -0,0 +1,20 @@
+package com.zarisa.bankaccountmanagement.data_base
+
+import androidx.room.*
+
+@Dao
+interface QuestionDao {
+    @Query("SELECT * FROM Account")
+    fun getAll(): List<Account>
+    @Query("SELECT * FROM Account where cardNumber=:cardNumber")
+    fun getAccount(cardNumber: Int): Account
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg account: Account)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(account: Account)
+
+    @Delete
+    fun delete(account: Account)
+
+}
